05a425880e804ff67694d5cb902bc80d87762ab5
Emre Berk Karakurt <laddentr2@gmail.com>
2022-06-16 18:58:59 +0300

    Şablon

--- Proje.py
+++ Proje.py
@@ -11,8 +11,15 @@ template = cv.imread("foto/burun.png") #Şablon==template, klasör/png dosyasın
 template_gray = cv.cvtColor(template, cv.COLOR_RGB2GRAY) #görüntünün gri tonlama modunda yüklenmesini belirtir.
    #cv.COLOR_RGB2GRAY ile kırmızı mavi yeşil renklerini gri olarak görecek.
 template_w, template_h = template_gray.shape[::-1] #w=genişlik, h=yükseklik (opencv'de wide'ı  ilk almamız gerekiyor)
-   #template_gray.shape şablonu kesiyoruz ve nereye bakılması gerektiğini gösteriyoruz -1 değeri tersine döndermemiz için.
-# oyun penceresi genişliği (x) yüksekliği girilmesi gerekiyor.,şüs
+   #template_gray.shape şablonu kesiyoruz ve nereye bakılması gerektiğini gösteriyoruz -1, değeri tersine döndermemiz için.
+
+# oyun penceresi genişliği,yüksekliği ve (x,y) girilmesi gerekiyor;
+x, y, w, h = 523, 247, 875, 679 #Ekranın kordinatları, capture'laması gereken ekranın yüzeyini belirlemek zorundayız.
+  #Yukarıdaki ekran kordinatları nasıl belirlenir;
+     #İlk önce tüm ekranımızı Paint'e aktarıyoruz, bu sayede ekranın sol alt'da px örneğin(212,899) 212=x 899=y olarak oyunun açıldığı yerdeki ekran kordinatlarını alıyoruz.
+     #(!!!) Ekran çözünürlüğüne göre bu değer değişir. Manuel olarak ayarlanması gerekebilir. 
+sleep(3) #Aktif etme tuşuna bastığımızda beklememiz gereken süre (3) bu 3 saniyelik aralıkta durumumuz: False 
+
 
 
 
